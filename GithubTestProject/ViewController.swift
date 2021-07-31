7ca34eca71358875b79e59a83eaf23e845cf55d0
alexdneprin <alexdneprin@gmail.com>
2021-07-31 11:51:20 +0300

    forth commit

--- GithubTestProject/ViewController.swift
+++ GithubTestProject/ViewController.swift
@@ -9,9 +9,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var centralView: UIView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
+        self.centralView.layer.cornerRadius = 30
     }
 
 
